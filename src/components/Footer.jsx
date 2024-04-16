7fd57c619b5245e2136b41385dbecb863be87ac5
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-04-16 02:48:56 -0300

    added initial footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
+import styles from "../style"
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from "../constants"
 
-
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div>
+        <img 
+            src={logo} 
+            alt="hoobank"
+            className="w-[266px] h-[72px] object-contain" 
+          />
+      </div>
+    </div>
+  </section>
+)
 
 export default Footer
