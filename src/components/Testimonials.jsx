1511d7b2419c906d2d7f3ecf340baa59adc1d695
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-04-16 00:19:36 -0300

    added initial testimonial strucuture

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,30 @@
+import { feedback, testimonials } from "../constants";
+import styles from "../style";
+import FeedBackCard from './FeedbackCard'
 
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
+const Testimonials = () => (
+   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    {/* TODO */}
+    <div />
+
+    {testimonials.map((testimonials) => (
+      <div key={testimonials.id} className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h1 className={styles.heading2}>{testimonials.content1} <br className="sm:block hidden"/> {testimonials.content2}</h1>
+        <div className="w-full md:mt-0 mt-6">
+         <p className={`${styles.paragraph} text-left max-w-[450px]`}>{testimonials.paragraph}</p>
+        </div>
+      </div>
+    ))}
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => (
+        <FeedBackCard key={card.id} {...card}/>
+      ))}
+    </div>
+    
+   </section>
   )
-}
+
 
 export default Testimonials
